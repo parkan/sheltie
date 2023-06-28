filecoin-project/lassie

feat: update event types to be discreet
In an effort to cut down on the unneccessary bloat some of our events started to have, this update removes the idea of event phases in favor of making the events discreet from each other. Events should not depend on each other and should not require data that is not relative to them.

cmd/lassie/fetch.go
@@ -261,31 +261,21 @@ type progressPrinter struct {
 
 func (pp *progressPrinter) subscriber(event types.RetrievalEvent) {
 	switch ret := event.(type) {
-	case events.RetrievalEventStarted:
-		switch ret.Phase() {
-		case types.IndexerPhase:
-			fmt.Fprintf(pp.writer, "\rQuerying indexer for %s...\n", ret.PayloadCid())
-		case types.QueryPhase:
-			fmt.Fprintf(pp.writer, "\rQuerying [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		case types.RetrievalPhase:
-			fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		}
-	case events.RetrievalEventConnected:
-		switch ret.Phase() {
-		case types.QueryPhase:
-			fmt.Fprintf(pp.writer, "\rQuerying [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		case types.RetrievalPhase:
-			fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		}
-	case events.RetrievalEventProposed:
-		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-	case events.RetrievalEventAccepted:
-		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-	case events.RetrievalEventFirstByte:
-		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-	case events.RetrievalEventCandidatesFound:
+	case events.StartedFindingCandidatesEvent:
+		fmt.Fprintf(pp.writer, "\rQuerying indexer for %s...\n", ret.PayloadCid())
+	case events.StartedRetrievalEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.ConnectedToSPEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.GraphsyncProposedEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.GraphsyncAcceptedEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.FirstByteEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.CandidatesFoundEvent:
 		pp.candidatesFound = len(ret.Candidates())
-	case events.RetrievalEventCandidatesFiltered:
+	case events.CandidatesFilteredEvent:
 		num := "all of them"
 		if pp.candidatesFound != len(ret.Candidates()) {
 			num = fmt.Sprintf("%d of them", len(ret.Candidates()))
@@ -300,13 +290,11 @@ func (pp *progressPrinter) subscriber(event types.RetrievalEvent) {
 		for _, candidate := range ret.Candidates() {
 			fmt.Fprintf(pp.writer, "\r\t%s, Protocols: %v\n", candidate.MinerPeer.ID, candidate.Metadata.Protocols())
 		}
-	case events.RetrievalEventFailed:
-		if ret.Phase() == types.IndexerPhase {
-			fmt.Fprintf(pp.writer, "\rRetrieval failure from indexer: %s\n", ret.ErrorMessage())
-		} else {
-			fmt.Fprintf(pp.writer, "\rRetrieval failure for [%s]: %s\n", types.Identifier(ret), ret.ErrorMessage())
-		}
-	case events.RetrievalEventSuccess:
+	case events.FailedEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieval failure from indexer: %s\n", ret.ErrorMessage())
+	case events.FailedRetrievalEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieval failure for [%s]: %s\n", events.Identifier(ret), ret.ErrorMessage())
+	case events.SucceededEvent:
 		// noop, handled at return from Retrieve()
 	}
 }

pkg/events/base.go
@@ -1,0 +1,52 @@
+package events
+
+import (
+	"time"
+
+	"github.com/filecoin-project/lassie/pkg/types"
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multicodec"
+)
+
+type retrievalEvent struct {
+	eventTime   time.Time
+	retrievalId types.RetrievalID
+	payloadCid  cid.Cid
+}
+
+func (r retrievalEvent) Time() time.Time                { return r.eventTime }
+func (r retrievalEvent) RetrievalId() types.RetrievalID { return r.retrievalId }
+func (r retrievalEvent) PayloadCid() cid.Cid            { return r.payloadCid }
+
+type spRetrievalEvent struct {
+	retrievalEvent
+	storageProviderId peer.ID
+}
+
+func (e spRetrievalEvent) StorageProviderId() peer.ID { return e.storageProviderId }
+
+type EventWithSPID interface {
+	types.RetrievalEvent
+	StorageProviderId() peer.ID
+}
+
+type EventWithCandidates interface {
+	types.RetrievalEvent
+	Candidates() []types.RetrievalCandidate
+}
+
+type EventWithProtocol interface {
+	types.RetrievalEvent
+	Protocol() multicodec.Code
+}
+
+type EventWithProtocols interface {
+	types.RetrievalEvent
+	Protocols() []multicodec.Code
+}
+
+type EventWithErrorMessage interface {
+	types.RetrievalEvent
+	ErrorMessage() string
+}

pkg/events/startedretrieval.go
@@ -1,0 +1,31 @@
+package events
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/filecoin-project/lassie/pkg/types"
+	"github.com/multiformats/go-multicodec"
+)
+
+var (
+	_ types.RetrievalEvent = StartedRetrievalEvent{}
+	_ EventWithSPID        = StartedRetrievalEvent{}
+	_ EventWithProtocol    = StartedRetrievalEvent{}
+)
+
+// StartedRetrievalEvent signals the start of a retrieval from a storage provider. It is emitted when the retrieval is started.
+type StartedRetrievalEvent struct {
+	spRetrievalEvent
+	protocol multicodec.Code
+}
+
+func (e StartedRetrievalEvent) Code() types.EventCode     { return types.StartedRetrievalCode }
+func (e StartedRetrievalEvent) Protocol() multicodec.Code { return e.protocol }
+func (e StartedRetrievalEvent) String() string {
+	return fmt.Sprintf("StartedRetrievalEvent<%s, %s, %s, %s, %s>", e.eventTime, e.retrievalId, e.payloadCid, e.storageProviderId, e.protocol)
+}
+
+func Started(at time.Time, retrievalId types.RetrievalID, candidate types.RetrievalCandidate, protocol multicodec.Code) StartedRetrievalEvent {
+	return StartedRetrievalEvent{spRetrievalEvent{retrievalEvent{at, retrievalId, candidate.RootCid}, candidate.MinerPeer.ID}, protocol}
+}

pkg/events/succeeded.go
@@ -1,0 +1,36 @@
+package events
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/filecoin-project/lassie/pkg/types"
+	"github.com/multiformats/go-multicodec"
+)
+
+var (
+	_ types.RetrievalEvent = SucceededEvent{}
+	_ EventWithSPID        = SucceededEvent{}
+	_ EventWithProtocol    = SucceededEvent{}
+)
+
+type SucceededEvent struct {
+	spRetrievalEvent
+	receivedBytesSize uint64
+	receivedCidsCount uint64
+	duration          time.Duration
+	protocol          multicodec.Code
+}
+
+func (e SucceededEvent) Code() types.EventCode     { return types.SuccessCode }
+func (e SucceededEvent) ReceivedBytesSize() uint64 { return e.receivedBytesSize }
+func (e SucceededEvent) ReceivedCidsCount() uint64 { return e.receivedCidsCount }
+func (e SucceededEvent) Duration() time.Duration   { return e.duration }
+func (e SucceededEvent) Protocol() multicodec.Code { return e.protocol }
+func (e SucceededEvent) String() string {
+	return fmt.Sprintf("SucceededEvent<%s, %s, %s, %s, { %d, %d, %s, %s }>", e.eventTime, e.retrievalId, e.payloadCid, e.storageProviderId, e.receivedBytesSize, e.receivedCidsCount, e.protocol, e.duration)
+}
+
+func Success(at time.Time, retrievalId types.RetrievalID, candidate types.RetrievalCandidate, receivedBytesSize uint64, receivedCidsCount uint64, duration time.Duration, protocol multicodec.Code) SucceededEvent {
+	return SucceededEvent{spRetrievalEvent{retrievalEvent{at, retrievalId, candidate.RootCid}, candidate.MinerPeer.ID}, receivedBytesSize, receivedCidsCount, duration, protocol}
+}

parkan/sheltie

cmd/lassie/fetch.go
@@ -140,31 +140,21 @@ type progressPrinter struct {
 
 func (pp *progressPrinter) subscriber(event types.RetrievalEvent) {
 	switch ret := event.(type) {
-	case events.RetrievalEventStarted:
-		switch ret.Phase() {
-		case types.IndexerPhase:
-			fmt.Fprintf(pp.writer, "\rQuerying indexer for %s...\n", ret.PayloadCid())
-		case types.QueryPhase:
-			fmt.Fprintf(pp.writer, "\rQuerying [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		case types.RetrievalPhase:
-			fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		}
-	case events.RetrievalEventConnected:
-		switch ret.Phase() {
-		case types.QueryPhase:
-			fmt.Fprintf(pp.writer, "\rQuerying [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		case types.RetrievalPhase:
-			fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-		}
-	case events.RetrievalEventProposed:
-		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-	case events.RetrievalEventAccepted:
-		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-	case events.RetrievalEventFirstByte:
-		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", types.Identifier(ret), ret.Code())
-	case events.RetrievalEventCandidatesFound:
+	case events.StartedFindingCandidatesEvent:
+		fmt.Fprintf(pp.writer, "\rQuerying indexer for %s...\n", ret.PayloadCid())
+	case events.StartedRetrievalEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.ConnectedToProviderEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.GraphsyncProposedEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.GraphsyncAcceptedEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.FirstByteEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieving from [%s] (%s)...\n", events.Identifier(ret), ret.Code())
+	case events.CandidatesFoundEvent:
 		pp.candidatesFound = len(ret.Candidates())
-	case events.RetrievalEventCandidatesFiltered:
+	case events.CandidatesFilteredEvent:
 		if len(fetchProviderAddrInfos) == 0 {
 			fmt.Fprintf(pp.writer, "Found %d storage provider candidate(s) in the indexer:\n", pp.candidatesFound)
 		} else {
@@ -173,13 +163,11 @@ func (pp *progressPrinter) subscriber(event types.RetrievalEvent) {
 		for _, candidate := range ret.Candidates() {
 			fmt.Fprintf(pp.writer, "\r\t%s, Protocols: %v\n", candidate.MinerPeer.ID, candidate.Metadata.Protocols())
 		}
-	case events.RetrievalEventFailed:
-		if ret.Phase() == types.IndexerPhase {
-			fmt.Fprintf(pp.writer, "\rRetrieval failure from indexer: %s\n", ret.ErrorMessage())
-		} else {
-			fmt.Fprintf(pp.writer, "\rRetrieval failure for [%s]: %s\n", types.Identifier(ret), ret.ErrorMessage())
-		}
-	case events.RetrievalEventSuccess:
+	case events.FailedEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieval failure from indexer: %s\n", ret.ErrorMessage())
+	case events.FailedRetrievalEvent:
+		fmt.Fprintf(pp.writer, "\rRetrieval failure for [%s]: %s\n", events.Identifier(ret), ret.ErrorMessage())
+	case events.SucceededEvent:
 		// noop, handled at return from Retrieve()
 	}
 }

pkg/events/base.go
@@ -1,0 +1,52 @@
+package events
+
+import (
+	"time"
+
+	"github.com/filecoin-project/lassie/pkg/types"
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multicodec"
+)
+
+type retrievalEvent struct {
+	eventTime   time.Time
+	retrievalId types.RetrievalID
+	payloadCid  cid.Cid
+}
+
+func (r retrievalEvent) Time() time.Time                { return r.eventTime }
+func (r retrievalEvent) RetrievalId() types.RetrievalID { return r.retrievalId }
+func (r retrievalEvent) PayloadCid() cid.Cid            { return r.payloadCid }
+
+type providerRetrievalEvent struct {
+	retrievalEvent
+	providerId peer.ID
+}
+
+func (e providerRetrievalEvent) ProviderId() peer.ID { return e.providerId }
+
+type EventWithProviderID interface {
+	types.RetrievalEvent
+	ProviderId() peer.ID
+}
+
+type EventWithCandidates interface {
+	types.RetrievalEvent
+	Candidates() []types.RetrievalCandidate
+}
+
+type EventWithProtocol interface {
+	types.RetrievalEvent
+	Protocol() multicodec.Code
+}
+
+type EventWithProtocols interface {
+	types.RetrievalEvent
+	Protocols() []multicodec.Code
+}
+
+type EventWithErrorMessage interface {
+	types.RetrievalEvent
+	ErrorMessage() string
+}

pkg/events/startedretrieval.go
@@ -1,0 +1,31 @@
+package events
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/filecoin-project/lassie/pkg/types"
+	"github.com/multiformats/go-multicodec"
+)
+
+var (
+	_ types.RetrievalEvent = StartedRetrievalEvent{}
+	_ EventWithProviderID  = StartedRetrievalEvent{}
+	_ EventWithProtocol    = StartedRetrievalEvent{}
+)
+
+// StartedRetrievalEvent signals the start of a retrieval from a storage provider. It is emitted when the retrieval is started.
+type StartedRetrievalEvent struct {
+	providerRetrievalEvent
+	protocol multicodec.Code
+}
+
+func (e StartedRetrievalEvent) Code() types.EventCode     { return types.StartedRetrievalCode }
+func (e StartedRetrievalEvent) Protocol() multicodec.Code { return e.protocol }
+func (e StartedRetrievalEvent) String() string {
+	return fmt.Sprintf("StartedRetrievalEvent<%s, %s, %s, %s, %s>", e.eventTime, e.retrievalId, e.payloadCid, e.providerId, e.protocol)
+}
+
+func StartedRetrieval(at time.Time, retrievalId types.RetrievalID, candidate types.RetrievalCandidate, protocol multicodec.Code) StartedRetrievalEvent {
+	return StartedRetrievalEvent{providerRetrievalEvent{retrievalEvent{at, retrievalId, candidate.RootCid}, candidate.MinerPeer.ID}, protocol}
+}

pkg/events/succeeded.go
@@ -1,0 +1,36 @@
+package events
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/filecoin-project/lassie/pkg/types"
+	"github.com/multiformats/go-multicodec"
+)
+
+var (
+	_ types.RetrievalEvent = SucceededEvent{}
+	_ EventWithProviderID  = SucceededEvent{}
+	_ EventWithProtocol    = SucceededEvent{}
+)
+
+type SucceededEvent struct {
+	providerRetrievalEvent
+	receivedBytesSize uint64
+	receivedCidsCount uint64
+	duration          time.Duration
+	protocol          multicodec.Code
+}
+
+func (e SucceededEvent) Code() types.EventCode     { return types.SuccessCode }
+func (e SucceededEvent) ReceivedBytesSize() uint64 { return e.receivedBytesSize }
+func (e SucceededEvent) ReceivedCidsCount() uint64 { return e.receivedCidsCount }
+func (e SucceededEvent) Duration() time.Duration   { return e.duration }
+func (e SucceededEvent) Protocol() multicodec.Code { return e.protocol }
+func (e SucceededEvent) String() string {
+	return fmt.Sprintf("SucceededEvent<%s, %s, %s, %s, { %d, %d, %s, %s }>", e.eventTime, e.retrievalId, e.payloadCid, e.providerId, e.receivedBytesSize, e.receivedCidsCount, e.protocol, e.duration)
+}
+
+func Success(at time.Time, retrievalId types.RetrievalID, candidate types.RetrievalCandidate, receivedBytesSize uint64, receivedCidsCount uint64, duration time.Duration, protocol multicodec.Code) SucceededEvent {
+	return SucceededEvent{providerRetrievalEvent{retrievalEvent{at, retrievalId, candidate.RootCid}, candidate.MinerPeer.ID}, receivedBytesSize, receivedCidsCount, duration, protocol}
+}