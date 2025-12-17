acb254f48d04f2b045684a4c9c5884962f53fc31
Arkadiy Kukarkin <parkan@gmail.com>
2025-12-17 18:32:48 +0100

    Remove unused carToDirEntry function

--- pkg/internal/itest/util.go
+++ pkg/internal/itest/util.go
@@ -4,9 +4,7 @@ import (
 	"io"
 	"testing"
 
-	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-unixfsnode"
-	unixfs "github.com/ipfs/go-unixfsnode/testutil"
 	"github.com/ipld/go-car/v2/storage"
 	"github.com/ipld/go-ipld-prime"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
@@ -23,9 +21,4 @@ func CarBytesLinkSystem(t *testing.T, carReader io.ReaderAt) ipld.LinkSystem {
 	linkSys.NodeReifier = unixfsnode.Reify
 	linkSys.TrustedStorage = true
 	return linkSys
-}
-
-// carToDirEntry is ToDirEntry but around a CAR body as the source of data.
-func carToDirEntry(t *testing.T, carReader io.ReaderAt, root cid.Cid, rootPath string, expectFull bool) unixfs.DirEntry {
-	return unixfs.ToDirEntryFrom(t, CarBytesLinkSystem(t, carReader), root, rootPath, expectFull)
 }
