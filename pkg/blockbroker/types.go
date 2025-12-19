e8333ed3d810b1cd24a7b2dafd60e02cae41aee6
Arkadiy Kukarkin <parkan@gmail.com>
2025-12-19 19:28:40 +0100

    add streaming extraction support via RetrieveAndExtract

--- pkg/blockbroker/types.go
+++ pkg/blockbroker/types.go
@@ -2,6 +2,7 @@ package blockbroker
 
 import (
 	"context"
+	"io"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
@@ -12,6 +13,9 @@ import (
 type BlockSession interface {
 	Get(ctx context.Context, c cid.Cid) (blocks.Block, error)
 	GetSubgraph(ctx context.Context, c cid.Cid, lsys linking.LinkSystem) (int, error)
+	// GetSubgraphStream returns a raw CAR stream for the given CID.
+	// Caller must close the returned ReadCloser.
+	GetSubgraphStream(ctx context.Context, c cid.Cid) (io.ReadCloser, string, error)
 	SeedProviders(ctx context.Context, c cid.Cid)
 	// UsedProviders returns the list of provider endpoints that served blocks
 	UsedProviders() []string
